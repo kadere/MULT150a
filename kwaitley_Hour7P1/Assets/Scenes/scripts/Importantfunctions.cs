f1dddcadf34e84f1b7683b69d8217be9e4203f48
kadere <78783915+kadere@users.noreply.github.com>
2025-03-13 13:22:02 -0700

    KwaitleyHour7_P1

---
+++ kwaitley_Hour7P1/Assets/Scenes/scripts/Importantfunctions.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Importantfunctions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    { int runspeed;
+      print ("This is called once a frame");  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
